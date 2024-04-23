795abdc7d9b3d9175ea8b1050bc1e1c024b53da4
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2024-04-23 22:17:41 +0800

    test

--- src/app/components/TagComponents/InputGender.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const InputGender = () => {
-  return <div>InputGender</div>;
-};
-
-export default InputGender;
--- src/app/store/actions/index.js
+++
@@ -1,4 +0,0 @@
-export const GET_USER = "getUser";
-export const LOGOUT = "logout";
-export const LOGIN = "login";
-export const GET_USER_INFORMATION = "getUserInformation";
